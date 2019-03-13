c0b5796d3bd9aca0238cb2789ec1ab1c044301bb
legend1b <olumideolayinka2@gmail.com>
2019-03-13 06:15:45 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,108 @@
+function adSetter(){
+// alert(navigator.userAgent);
+var admobid = {};
+// select the right Ad Id according to platform
+if( /(android)/i.test(navigator.userAgent) ) { 
+    admobid = { // for Android
+        banner: 'ca-app-pub-5229881275738538/6969723872',
+        interstitial: 'ca-app-pub-5229881275738538/7521044752'
+    };
+} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+    admobid = { // for iOS
+        banner: 'ca-app-pub-5229881275738538/6969723872',
+        interstitial: 'ca-app-pub-5229881275738538/7521044752'
+    };
+} else {
+    admobid = { // for Windows Phone
+        banner: 'ca-app-pub-5229881275738538/6969723872',
+        interstitial: 'ca-app-pub-5229881275738538/7521044752'
+    };
+}
+
+if(window.AdMob) AdMob.createBanner( {
+    adId:admobid.banner, 
+    //isTesting:true,
+    position:AdMob.AD_POSITION.TOP_LEFT, 
+    autoShow:true} );
+	
+if(window.AdMob) AdMob.createBanner( {
+    adId:admobid.banner, 
+    //isTesting:true,
+    position:AdMob.AD_POSITION.TOP_CENTER, 
+    autoShow:true} );
+	
+if(window.AdMob) AdMob.createBanner( {
+    adId:admobid.banner, 
+    //isTesting:true,
+    position:AdMob.AD_POSITION.TOP_RIGHT, 
+    autoShow:true} );
+
+if(window.AdMob) AdMob.createBanner( {
+    adId:admobid.banner, 
+    //isTesting:true,
+    position:AdMob.AD_POSITION.MIDDLE_LEFT, 
+    autoShow:true} );
+	
+if(window.AdMob) AdMob.createBanner( {
+    adId:admobid.banner, 
+    //isTesting:true,
+    position:AdMob.AD_POSITION.MIDDLE_CENTER, 
+    autoShow:true} );
+	
+if(window.AdMob) AdMob.createBanner( {
+    adId:admobid.banner, 
+    //isTesting:true,
+    position:AdMob.AD_POSITION.MIDDLE_RIGHT, 
+    autoShow:true} );
+	
+if(window.AdMob) AdMob.createBanner( {
+    adId:admobid.banner, 
+    //isTesting:true,
+    position:AdMob.AD_POSITION.BOTTOM_LEFT, 
+    autoShow:true} );
+	
+if(window.AdMob) AdMob.createBanner( {
+    adId:admobid.banner, 
+    //isTesting:true,
+    position:AdMob.AD_POSITION.BOTTOM_CENTER, 
+    autoShow:true} );
+	
+if(window.AdMob) AdMob.createBanner( {
+    adId:admobid.banner, 
+    //isTesting:true,
+    position:AdMob.AD_POSITION.BOTTOM_RIGHT, 
+    autoShow:true} );
+	
+if(window.AdMob) AdMob.createBanner( {
+    adId:admobid.banner, 
+    //isTesting:true,
+    position:AdMob.AD_POSITION.TOP_APP, 
+    autoShow:true} );
+	
+if(window.AdMob) AdMob.createBanner( {
+    adId:admobid.banner, 
+    //isTesting:true,
+    position:AdMob.AD_POSITION.BOTTOM_APP, 
+    autoShow:true} );
+
+  if(window.AdMob) AdMob.prepareInterstitial( {adId:admobid.interstitial, autoShow:false} );
+
+
+}
+  function onDeviceReady(){
+  StatusBar.backgroundColorByHexString("#820035");
+   alert("device ready");
+      adSetter();
+  }
+function intershoo(){
+	if(AdMob) AdMob.showInterstitial();
+}
+
+function intershow(){
+	alert("device ready");
+      adSetter();
+}
+
+function domLoaded(){
+ document.addEventListener("deviceready", onDeviceReady, false);
+}
